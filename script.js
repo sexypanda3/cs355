52b9f0f29ec60a98021094229eed5a013c6a28d4
sexypanda3 <112658891+sexypanda3@users.noreply.github.com>
2023-02-07 15:13:25 -0500

    VSCode bata nabhako awasta

---
+++ script.js
@@ -0,0 +1,8 @@
+function hero(newColor) {
+    const elem = document.getElementById('abcd');
+    elem.style.color = newColor;
+  }
+  function ero(newColor) {
+    const elem = document.getElementById('abcd');
+    elem.style.backgroundColor = newColor;
+  }
